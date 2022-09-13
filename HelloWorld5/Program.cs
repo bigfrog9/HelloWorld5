c095649a276376ecd8b346f135f10c6b66dc417b
John Wohler <johndoubleyou11@gmail.com>
2022-09-13 09:29:53 -0300

    Initial commit

---
+++ HelloWorld5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 5");
+            Console.WriteLine("It's John, I put my consciousness in the computer!!");
+            Console.WriteLine("" + "");
+            Console.WriteLine("" + "");
+            Console.WriteLine("" + "");
+            Console.WriteLine("" + "");
+            Console.WriteLine("" + "");
+            Console.WriteLine("Just Kidding");
+            Console.ReadKey(true);
+        }
+    }
+}
